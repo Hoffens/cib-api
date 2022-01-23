12210dbfdcf81091f333f35ccc29d9aaec754dc9
Martin Cespedes <mcespedescubillos@gmail.com>
2022-01-22 23:25:16 -0300

    Se agrega funcion para solo una fila

--- src/service/to_json.py
+++ src/service/to_json.py
@@ -1,3 +1,10 @@
 def query_to_json_list(cursor):
     return [dict((cursor.description[i][0], value) \
             for i, value in enumerate(row)) for row in cursor.fetchall()]
+
+def query_to_json(cursor):
+    row = cursor.fetchone()
+    d = {}
+    for i in range(len(row)):
+        d[cursor.description[i][0]] = row[i]
+    return d
