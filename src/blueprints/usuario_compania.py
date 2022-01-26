44d9aa511fb9697aee060b6fcb5e084420173cf7
Martin Cespedes <mcespedescubillos@gmail.com>
2022-01-25 22:54:10 -0300

    Actualizar requiere todas las propiedades

--- src/blueprints/usuario_compania.py
+++ src/blueprints/usuario_compania.py
@@ -8,7 +8,7 @@ from extensions import db
 
 usuario_compania = Blueprint('usuario_compania', __name__)
 
-crear_usuario_compania_schema = {
+usuario_compania_schema = {
     "type" : "object",
     "properties" : {
         "rut" : {"type" : "number"},
@@ -23,7 +23,7 @@ crear_usuario_compania_schema = {
         },
         "correo" : {"type" : "string"},
         "telefono" : {"type" : "string"},
-        "grupo_sanguineo" : {"type" : "number"},
+        "grupo_sanguineo" : {"type" : ["number", "null"]},
         "activo" : {"type" : "boolean"},
         "rut_cuenta": {"type": "number"}
     },
@@ -31,47 +31,12 @@ crear_usuario_compania_schema = {
                 "fecha_nacimiento", "correo", "activo", "rut_cuenta"]
 }
 
-actualizar_usuario_compania_schema = {
-    "type" : "object",
-    "required": ["rut", "rut_cuenta"],
-    "anyOf": [
-        {"required": ["password"]},
-        {"required": ["rol"]},
-        {"required": ["nombre"]},
-        {"required": ["apellido_paterno"]},
-        {"required": ["apellido_materno"]},
-        {"required": ["fecha_nacimiento"]},
-        {"required": ["correo"]},
-        {"required": ["telefono"]},
-        {"required": ["grupo_sanguineo"]},
-        {"required": ["activo"]}
-    ],
-    "properties" : {
-        "rut" : {"type" : "number"},
-        "password" : {"type" : "string"},
-        "rol": {"type" : "number"},
-        "nombre" : {"type" : "string"},
-        "apellido_paterno" : {"type" : "string"},
-        "apellido_materno" : {"type" : "string"},
-        "fecha_nacimiento": {
-            "type": "string",
-            "format": "date"
-        },
-        "correo" : {"type" : "string"},
-        "telefono" : {"type" : "string"},
-        "grupo_sanguineo" : {"type" : "number"},
-        "activo" : {"type" : "boolean"},
-        "rut_cuenta": {"type": "number"}
-    }
-}
-
-
 @usuario_compania.route('/api/usuario_compania', methods=['POST'])
 #@token_required
 def crear_usuario_compania():
     try:
         data = request.get_json()
-        validate(instance=data, schema=crear_usuario_compania_schema)
+        validate(instance=data, schema=usuario_compania_schema)
         query = f"select compania, rol from usuario_compania where rut = {data['rut_cuenta']}"
         cursor = db.connection.cursor()
         cursor.execute(query)
@@ -90,27 +55,46 @@ def crear_usuario_compania():
         if user is None:
             hashed_password = bcrypt.hashpw(data['password'].encode("utf-8"), bcrypt.gensalt())
             # Se ingresa la informacion obligatoria
-            query = f"""INSERT INTO usuario_compania (rut, compania, rol, nombre, apellido_paterno, apellido_materno, fecha_nacimiento, correo,
-                    fecha_ingreso, u_password, activo) VALUES ({data['rut']}, '{compania}', {data['rol']}, 
-                    '{data['nombre']}', '{data['apellido_paterno']}', '{data['apellido_materno']}', date('{data['fecha_nacimiento']}'), 
-                    '{data['correo']}', CURDATE(), '{hashed_password.decode('utf-8')}', 1);"""
+            query = f"""
+                    INSERT INTO
+                      usuario_compania (
+                        rut,
+                        compania,
+                        rol,
+                        nombre,
+                        apellido_paterno,
+                        apellido_materno,
+                        fecha_nacimiento,
+                        correo,
+                        fecha_ingreso,
+                        u_password,
+                        activo,
+                        telefono,
+                        grupo_sanguineo
+                      )
+                    VALUES
+                      (
+                        {data['rut'] },
+                        '{compania}',
+                        { data['rol'] },
+                        '{data['nombre']}',
+                        '{data['apellido_paterno']}',
+                        '{data['apellido_materno']}',
+                        date('{data['fecha_nacimiento']}'),
+                        '{data['correo']}',
+                        CURDATE(),
+                        '{hashed_password.decode('utf -8')}',
+                        1
+                        '{data['telefono']}',
+                        {data['grupo_sanguineo']}
+                      );
+            """
             cursor.execute(query)
             db.connection.commit()
-
-            if "telefono" in data:
-                query = f"insert into usuario (telefono) values ('{data['telefono']}');"
-                cursor.execute(query)
-                db.connection.commit()
-
-            if "grupo_sanguineo" in data:
-                query = f"insert into usuario (grupo_sanguineo) values ({data['grupo_sanguineo']});"
-                cursor.execute(query)
-                db.connection.commit()
-
             cursor.close()
-            return jsonify({ 'status': 'Ok', 'message' : 'usuario_compania creado correctamente.' }), 200
+            return jsonify({ 'status': 'Ok', 'message' : 'Usuario compania creado correctamente.' }), 200
 
-        return jsonify({ 'status': 'Error', 'message' : 'usuario_compania ya existe.' }), 422
+        return jsonify({ 'status': 'Error', 'message' : 'Usuario compania ya existe.' }), 422
         
     except:
         return jsonify({ 'status': 'Error', 'message' : 'Error inesperado, verifique que la información cargada sea correcta.' }), 500
@@ -121,7 +105,7 @@ def crear_usuario_compania():
 def actualizar_usuario_compania_compania():
     try:
         data = request.get_json()        
-        validate(instance=data, schema=actualizar_usuario_compania_schema)
+        validate(instance=data, schema=usuario_compania_schema)
 
         query = f"select compania, rol from usuario_compania where rut = {data['rut_cuenta']}"
         cursor = db.connection.cursor()
@@ -138,58 +122,28 @@ def actualizar_usuario_compania_compania():
         user = cursor.fetchone()
 
         if user:
-            if 'password' in data:
-                hashed_password = bcrypt.hashpw(
-                    data['password'].encode("utf-8"), bcrypt.gensalt())
-                query = f"update usuario set password = '{hashed_password.decode('utf-8')}'"
-                cursor.execute(query)
-                db.connection.commit()
-
-            if 'rol' in data:
-                query = f"update usuario set rol = {data['rol']}"
-                cursor.execute(query)
-                db.connection.commit()
-
-            if 'nombre' in data:
-                query = f"update usuario set nombre = '{data['nombre']}'"
-                cursor.execute(query)
-                db.connection.commit()
-
-            if 'apellido_paterno' in data:
-                query = f"update usuario set apellido_paterno = '{data['apellido_paterno']}'"
-                cursor.execute(query)
-                db.connection.commit()
-
-            if 'apellido_materno' in data:
-                query = f"update usuario set apellido_materno = '{data['apellido_materno']}'"
-                cursor.execute(query)
-                db.connection.commit()
-
-            if 'fecha_nacimiento' in data:
-                query = f"update usuario set fecha_nacimiento = '{data['fecha_nacimiento']}'"
-                cursor.execute(query)
-                db.connection.commit()
-
-            if 'correo' in data:
-                query = f"update usuario set correo = '{data['correo']}'"
-                cursor.execute(query)
-                db.connection.commit()
-
-            if 'telefono' in data:
-                query = f"update usuario set telefono = '{data['telefono']}'"
-                cursor.execute(query)
-                db.connection.commit()
-
-            if 'grupo_sanguineo' in data:
-                query = f"update usuario set grupo_sanguineo = {data['grupo_sanguineo']}"
-                cursor.execute(query)
-                db.connection.commit()
-
-            if 'activo' in data:
-                query = f"update usuario set activo = {data['activo']}"
-                cursor.execute(query)
-                db.connection.commit()
-
+            hashed_password = bcrypt.hashpw(data['password'].encode("utf-8"), bcrypt.gensalt())
+            query = f"""
+                        update
+                          usuario
+                        set
+                          rut = {data['rut']},
+                          u_password = '{hashed_password.decode('utf-8')}',
+                          compania = {compania},
+                          rol = {data['rol']},
+                          nombre = '{data['nombre']}',
+                          apellido_paterno = '{data['apellido_paterno']}',
+                          apellido_materno = '{data['apellido_materno']}',
+                          fecha_nacimiento = '{data['fecha_nacimiento']}',
+                          correo = '{data['correo']}',
+                          telefono = '{data['telefono']}',
+                          grupo_sanguineo = % s,
+                          activo = {data['activo']}
+                        where
+                          rut = {data['rut']};
+                    """
+            cursor.execute(query, (data['grupo_sanguineo'], ))
+            db.connection.commit()
             cursor.close()
 
             return jsonify({ 'status': 'Ok', 'message' : 'usuario_compania actualizado correctamente.'}), 200
